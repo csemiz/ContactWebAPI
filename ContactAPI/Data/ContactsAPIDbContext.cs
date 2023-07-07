8965574f4bd20f02dee4a5b0599178ff44062480
csemiz <ceren_semiz@outlook.com>
2023-07-07 18:03:38 +0300

    Add project files.

---
+++ ContactAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Data
+{
+	public class ContactsAPIDbContext:DbContext
+	{
+        public ContactsAPIDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
